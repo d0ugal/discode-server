0a35621739e4de93a7d655c53eb30116a1fa9e71
Dougal Matthews <dougal@redhat.com>
2017-04-14 12:06:14 +0100

    Further test coverage of the db api

--- discode_server/tests/test_db.py
+++ discode_server/tests/test_db.py
@@ -6,10 +6,17 @@ from discode_server import db as dbapi
 
 
 @pytest.mark.asyncio
-async def test_get_paste(db):
+async def test_get_paste_error(db):
     async with db.acquire() as conn:
         with pytest.raises(dbapi.PasteNotFound):
             await dbapi.get_paste(conn, 1)
+
+
+@pytest.mark.asyncio
+async def test_get_pastes_error(db):
+    async with db.acquire() as conn:
+        with pytest.raises(dbapi.PasteNotFound):
+            await dbapi.get_pastes(conn)
 
 
 @pytest.mark.asyncio
@@ -44,3 +51,33 @@ async def test_delete_expired(db):
         await dbapi.delete_expired(conn)
         pastes = await dbapi.get_pastes(conn)
         assert ["2"] == [p.id for p in pastes]
+
+
+@pytest.mark.asyncio
+async def test_get_comments_empty(db):
+    async with db.acquire() as conn:
+        comments = await dbapi.get_comments(conn, 1)
+        assert comments == []
+
+
+@pytest.mark.asyncio
+async def test_create_comment(db):
+    async with db.acquire() as conn:
+        await dbapi.create(conn, "PASTE 1 CONTENTS", "python")
+        await dbapi.create_comment(conn, 1, 1, "TESTING")
+        comments = await dbapi.get_comments(conn, 1)
+        assert [c.id for c in comments] == [1, ]
+
+
+@pytest.mark.asyncio
+async def test_create_paste_and_comments(db):
+    async with db.acquire() as conn:
+        await dbapi.create(conn, "PASTE CONTENTS", "python")
+        await dbapi.create_comment(conn, 1, 1, "TESTING")
+        paste = await dbapi.get_paste(conn, 1)
+        assert paste.id == '1'
+        assert paste.decimal_id == 1
+        assert paste.contents == "PASTE CONTENTS"
+        assert paste.lexer == "python"
+        assert paste.created_on.date() == datetime.date.today()
+        assert paste.comments == {1: ["TESTING", ]}
