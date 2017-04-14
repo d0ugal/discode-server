1ae4307034d1e32b8528130f3d52b1ac3516bf13
Dougal Matthews <dougal@redhat.com>
2017-04-14 11:06:15 +0100

    Revert "Working view tests!"
    This reverts commit 9514f54f6a922f93081eab28049a687d86f435de.

--- discode_server/tests/test_views.py
+++ discode_server/tests/test_views.py
@@ -1,3 +1,7 @@
-async def test_index(test_client):
-    request, response = await test_client.get("/")
+import pytest
+
+
+@pytest.mark.skip(reason="I don't know why this fails.")
+def test_index(test_client):
+    request, response = test_client.get("/")
     assert response.status == 200
